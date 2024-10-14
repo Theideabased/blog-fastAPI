b0375d70c571e1d8f22cb0aa4d25092a14d99a36
Theideabased <ogunmusireseyi01@gmail.com>
2024-10-14 12:29:58 +0100

    building a blog website APIs

---
+++ blog/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+import schemas
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+app = FastAPI()
+
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db:Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id ==
+                                 id).delete(synchronize_session=False)
+    db.commit()
+    return {"details": f"The blog with the {id} is deleted"}
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def show(id,response: Response, db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Blog with the {id} is not available")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return{'details': f"Blog with the {id} is not available"}
+    return blog
