c632bb7234f0465e78548666dbbbaf1ec15f7cb1
Theideabased <ogunmusireseyi01@gmail.com>
2024-10-14 17:18:12 +0100

    learning to build blogging platform using fastapi

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
+from typing import List
 import schemas
 import models
 from database import engine, SessionLocal
@@ -7,7 +8,7 @@ app = FastAPI()
 
 
 models.Base.metadata.create_all(engine)
-
+# models.Base.metadata.drop_all(engine, tables=[models.User.__table__])
 def get_db():
     db = SessionLocal()
     try:
@@ -30,12 +31,23 @@ def destroy(id, db:Session = Depends(get_db)):
                                  id).delete(synchronize_session=False)
     db.commit()
     return {"details": f"The blog with the {id} is deleted"}
-@app.get('/blog')
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.update(request.model_dump())
+    db.commit()
+    return 'updated'
+
+
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id,response: Response, db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -44,3 +56,11 @@ def show(id,response: Response, db:Session = Depends(get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return{'details': f"Blog with the {id} is not available"}
     return blog
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
