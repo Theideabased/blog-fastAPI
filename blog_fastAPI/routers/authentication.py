505b0c1bd75490b26f40435c476a2d0a430bfddd
Theideabased <ogunmusireseyi01@gmail.com>
2024-10-16 17:31:25 +0100

    jwt_token problem

--- blog_fastAPI/routers/authentication.py
+++ blog_fastAPI/routers/authentication.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
+from ..schemas import Token
+from ..jwt_token import create_access_token
 from .. import schemas, database, models
 from .. hashing import Hash
+from fastapi.security import OAuth2PasswordRequestForm
 router = APIRouter(
     tags=['Authentication']
 )
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session=Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session=Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -15,4 +18,7 @@ def login(request: schemas.Login, db: Session=Depends(database.get_db)):
     if not Hash.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'incorrect password')
-    return user
+    access_token = create_access_token(
+        data={"sub": user.email}
+    )
+    return {"access_token": access_token, "token_type" : "bearer"}
--- blog_fastAPI/routers/blog.py
+++ blog_fastAPI/routers/blog.py
@@ -3,15 +3,14 @@ from blog_fastAPI import schemas, database
 from sqlalchemy.orm import Session
 from typing import List
 from blog_fastAPI.utils import blog
-
-
+from ..oauth2 import get_current_user
 router = APIRouter(
     prefix='/blog',
     tags=['blogs']
 )
 
 @router.get('/', response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(database.get_db)):
+def all(db: Session = Depends(database.get_db),get_current_user: schemas.User = Depends(get_current_user)):
     return blog.get_all(db)
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
